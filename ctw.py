797c5c214e5aee853b71dc274dcfa801507d4662
hardworkar <arsmoroz@ya.ru>
2021-08-09 17:54:19 +0700

    пока как-то не сжимается штука

--- ctw.py
+++ ctw.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import math
 
-enwik4 = open("enwik4", 'rb').read()
+enwik4 = open("enwik4", 'rb').read()[:100]
 
 
 def bitgen(x):
@@ -63,37 +63,52 @@ exit(0)
 
 
 
-from collections import defaultdict
-lookup = defaultdict(lambda: [0, 0])
 
 bg = bitgen(enwik4)
-BACK_BITS = 16 
+context_bits = 16 
 H = 0.0
+
+from model import CTW
+ctw = CTW(context_bits = context_bits)
 
 enc = Coder()
 
 try:
-  prevx = [-1]*BACK_BITS
   while 1:
-    x = next(bg)
-    px = tuple(prevx)
-    # https://en.wikipedia.org/wiki/Krichevsky%E2%80%93Trofimov_estimator
-    p_x = (lookup[px][x] + 0.5) / (lookup[px][0]+lookup[px][1]+1)
-    p_0 = (lookup[px][0] + 0.5) / (lookup[px][0]+lookup[px][1]+1)
+    pwn = ctw.getLogPn()
 
+    # dummy 0-update
+    ctw.update(0)
+    pwn0 = ctw.getLogPn()
+    ctw.save_load(save = False)
+
+    # dummy 1-update
+    ctw.update(1)
+    pwn1 = ctw.getLogPn()
+    ctw.save_load(save = False)
+
+    p_0 = math.exp(pwn0 - pwn)
+    p_1 = math.exp(pwn1 - pwn)
+    print("p_0: " + str(p_0))
+
+    assert (p_0 + p_1 - 1 < 1e-1)
+
+    x = next(bg)
+    ctw.update(x)
+    ctw.save_load(save = True)
+
+    p_x = p_0 if x == 0 else 1 - p_0
+        
     enc.code(p_0, x)
 
     H += -math.log2(p_x)
 
-    lookup[px][x] += 1
-     
-    prevx.append(x)
-    prevx = prevx[-BACK_BITS:]
 
 except StopIteration:
   pass
 
 print("%.2f bytes of entropy" % (H / 8))
+exit(0)
 
 def write_bin_from_bitstream(fn, bitstream):
   ob = []
--- model.py
+++ model.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python3    
+import math
 class Node():
   def __init__(self, parent = None, depth = 0):
-    self.c = [0] * 2
     self.parent = parent
     self.child = [None] * 2
+    self.old_child = [None] * 2
     self.depth = depth
-    self.pe = 1
-    self.pw = 1
+
+    # "serializable"
+    self.c = [0] * 2
+    self.old_c = [0] * 2
+    self.pe = self.old_pe = 0
+    self.pw = self.old_pw = 0
+
     
 
 class CTW():
@@ -17,16 +23,32 @@ class CTW():
     self.root = Node()
     self.prevx = prevx
 
+  # idk
+  def save_load(self, node = None, save = True):
+    if node == None:
+      node = self.root
+
+    if save:
+      node.old_c[0], node.old_c[1], node.old_pe, node.old_pw = node.c[0], node.c[1], node.pe, node.pw
+      node.old_child[0], node.old_child[1] = node.child[0], node.child[1]
+    else:
+      node.c[0], node.c[1], node.pe, node.pw = node.old_c[0], node.old_c[1], node.old_pe, node.old_pw
+      node.child[0], node.child[1] = node.old_child[0], node.old_child[1]
+
+    for i in range(2):
+      if node.child[i] != None:
+        self.save_load(node.child[i])
+
   def update(self, x):
     node = self.root
-    node.pe = (node.c[x] + 0.5) / (node.c[0] + node.c[1] + 1) * node.pe
+    node.pe = math.log(node.c[x] + 0.5) - math.log(node.c[0] + node.c[1] + 1) + node.pe
     node.c[x] += 1
     for i in range(len(self.prevx) - 1, -1, -1):
       last = self.prevx[i]
       if node.child[last] == None:
         node.child[last] = Node(parent = node, depth = node.depth + 1)
       node = node.child[last] 
-      node.pe = (node.c[x] + 0.5) / (node.c[0] + node.c[1] + 1) * node.pe
+      node.pe = math.log(node.c[x] + 0.5) - math.log(node.c[0] + node.c[1] + 1) + node.pe
       node.c[x] += 1
 
     # node is leaf
@@ -34,12 +56,15 @@ class CTW():
     node.pw = node.pe
     while node.parent != None:
       node = node.parent
-      pw0s = node.child[0].pw if node.child[0] != None  else 1
-      pw1s = node.child[1].pw if node.child[1] != None  else 1
-      node.pw = 0.5 * (node.pe + pw0s * pw1s)
+      pw0s = node.child[0].pw if node.child[0] != None else 0
+      pw1s = node.child[1].pw if node.child[1] != None else 0
+      node.pw = math.log(0.5) + math.log(math.exp(node.pe) + math.exp(pw0s + pw1s))
 
     self.prevx = self.prevx[1:]
     self.prevx.append(x)
+
+  def getLogPn(self):
+    return self.root.pw
 
   def printTree(self, node = None, suff = []):
     if node == None:
@@ -48,14 +73,3 @@ class CTW():
     for i in range(2):
       if node.child[i] != None:
         self.printTree(node = node.child[i], suff = [i] + suff)
-
-
-
-
-context = [0, 1, 0]
-h = [0, 1, 1, 0, 1, 0, 0]
-
-model = CTW(prevx = context)
-for x in h:
-  model.update(x) 
-model.printTree()
