6a0d2928f530a5cebe0fe2655f57b2e1406d1adf
hardworkar <arsmoroz@ya.ru>
2021-08-10 12:59:17 +0700

    вроде работает, но возможно траверсить полное двоичное дерево не стоит

--- ctw.py
+++ ctw.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 import math
 
-enwik4 = open("enwik4", 'rb').read()[:100]
+enwik4 = open("enwik4", 'rb').read()[:1000]
+print(enwik4)
 
 
 def bitgen(x):
@@ -14,56 +15,6 @@ from coder import Coder
 import itertools
 import numpy as np
 
-
-'''
-p_0 = 0.009
-n = 80000 
-k = int(n * (1 - p_0))
-
-def rand_bin_array(K, N):
-  arr = np.zeros(N)
-  arr[:K]  = 1
-  np.random.shuffle(arr)
-  return arr
-
-seqs = [rand_bin_array(k, n)]
-
-# seqs = list(map(list, itertools.product([0, 1], repeat = n)))
-print(seqs)
-
-errors = 0
-
-for seq in seqs:
-  enc = Coder()
-  for x in seq:
-    enc.code(p_0, x)
-  coded = enc.getCoded()
-
-  dec = Coder(ob = coded)
-  decoded = []
-  try:
-    while 1:
-      x = dec.code(p_0)
-      decoded.append(x)
-      # feels wrong, but...
-      if len(decoded) == len(seq):
-        break
-  except StopIteration:
-    pass
-  if len ([i for i, j in zip(seq, decoded) if i == j]) != len(seq):
-     print("orig: " + str(seq)) 
-     print("code: " + str(coded)) 
-     print("deco: " + str(decoded) + "\n") 
-     errors += 1
-print("TOTAL ERRS: %d" % errors)
-
-exit(0)
-'''
-
-
-
-
-
 bg = bitgen(enwik4)
 context_bits = 16 
 H = 0.0
@@ -75,27 +26,16 @@ enc = Coder()
 
 try:
   while 1:
-    pwn = ctw.getLogPn()
+    p_0 = math.exp(ctw.getLogPx(0))
+    p_1 = math.exp(ctw.getLogPx(1))
 
-    # dummy 0-update
-    ctw.update(0)
-    pwn0 = ctw.getLogPn()
-    ctw.save_load(save = False)
+    # print("p_0: " + str(p_0))
+    # print("p_1: " + str(p_1))
 
-    # dummy 1-update
-    ctw.update(1)
-    pwn1 = ctw.getLogPn()
-    ctw.save_load(save = False)
-
-    p_0 = math.exp(pwn0 - pwn)
-    p_1 = math.exp(pwn1 - pwn)
-    print("p_0: " + str(p_0))
-
-    assert (p_0 + p_1 - 1 < 1e-1)
+    assert (abs(p_0 + p_1 - 1) < 1e-6)
 
     x = next(bg)
     ctw.update(x)
-    ctw.save_load(save = True)
 
     p_x = p_0 if x == 0 else 1 - p_0
         
--- model.py
+++ model.py
@@ -1,17 +1,21 @@
 #!/usr/bin/env python3    
 import math
+import numpy as np
 class Node():
-  def __init__(self, parent = None, depth = 0):
+  def __init__(self, parent = None):
     self.parent = parent
     self.child = [None] * 2
     self.old_child = [None] * 2
-    self.depth = depth
+    if parent != None:
+      self.depth = parent.depth + 1
+    else:
+      self.depth = 0
 
     # "serializable"
     self.c = [0] * 2
     self.old_c = [0] * 2
-    self.pe = self.old_pe = 0
-    self.pw = self.old_pw = 0
+    self.pe = self.old_pe = 0.0
+    self.pw = self.old_pw = 0.0
 
     
 
@@ -37,39 +41,77 @@ class CTW():
 
     for i in range(2):
       if node.child[i] != None:
-        self.save_load(node.child[i])
+        self.save_load(node.child[i], save)
 
-  def update(self, x):
+  def update(self, x, tmp = False):
     node = self.root
-    node.pe = math.log(node.c[x] + 0.5) - math.log(node.c[0] + node.c[1] + 1) + node.pe
+    node.pe += math.log(node.c[x] + 0.5) - math.log(sum(node.c) + 1.0)
     node.c[x] += 1
     for i in range(len(self.prevx) - 1, -1, -1):
+      # go one step Dipper
       last = self.prevx[i]
       if node.child[last] == None:
-        node.child[last] = Node(parent = node, depth = node.depth + 1)
+        node.child[last] = Node(parent = node)
       node = node.child[last] 
-      node.pe = math.log(node.c[x] + 0.5) - math.log(node.c[0] + node.c[1] + 1) + node.pe
+
+      # update node
+      node.pe += math.log(node.c[x] + 0.5) - math.log(sum(node.c) + 1.0)
       node.c[x] += 1
 
     # node is leaf
-    # back-propagate!
+    assert node.depth == self.context_bits
     node.pw = node.pe
+    # back-propagate!
     while node.parent != None:
       node = node.parent
-      pw0s = node.child[0].pw if node.child[0] != None else 0
-      pw1s = node.child[1].pw if node.child[1] != None else 0
-      node.pw = math.log(0.5) + math.log(math.exp(node.pe) + math.exp(pw0s + pw1s))
+      assert node.depth < self.context_bits
+      pw0s = node.child[0].pw if node.child[0] != None else 0.0
+      pw1s = node.child[1].pw if node.child[1] != None else 0.0
+      node.pw = math.log(0.5) + np.logaddexp(node.pe, pw0s + pw1s)
+    if not tmp:
+      self.prevx = self.prevx[1:]
+      self.prevx.append(x)
 
-    self.prevx = self.prevx[1:]
-    self.prevx.append(x)
+  def getLogPx(self, x):
+    # save everything
+    pw = self.root.pw
+    self.save_load(save = True)
 
-  def getLogPn(self):
-    return self.root.pw
+    # dummy x-update
+    self.update(x, tmp = True)
+    pwx = self.root.pw
+    # print("for %d, pwx: %.6f" % (x, math.exp(pwx)))
+
+    # restore everything
+    self.save_load(save = False)
+
+    # log (/) = -
+    return pwx - pw
 
   def printTree(self, node = None, suff = []):
     if node == None:
       node = self.root
-    print("node %s: (%d %d), %.6f %.6f" % (str(suff), node.c[0], node.c[1], node.pe, node.pw)) 
+    print("node %s: (%d %d), %.6f %.6f" % (str(suff), node.c[0], node.c[1], math.exp(node.pe), math.exp(node.pw))) 
     for i in range(2):
       if node.child[i] != None:
         self.printTree(node = node.child[i], suff = [i] + suff)
+'''
+
+context = [0, 1, 0]
+h = [0, 1, 1, 0, 1, 0, 0]
+h = [1]
+model = CTW(prevx = context)
+for x in h:
+  # print("p_0: " + str(math.exp(model.getLogPx(0))))
+  # print("p_1: " + str(math.exp(model.getLogPx(1))))
+  print()
+  model.update(x) 
+  print("we got %d" % x)
+print("pw: %.6f" % math.exp(model.root.pw))
+print("Tree before:")
+model.printTree()
+print("p_1: " + str(math.exp(model.getLogPx(1))))
+print("Tree after p_1:")
+model.printTree()
+print("p_0: " + str(math.exp(model.getLogPx(0))))
+'''
